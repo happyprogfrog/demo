0bac54d565f795fc81cd7c642b7c824bbc2e3433
happyprogfrog <happyprogfrog@gmail.com>
2025-08-03 21:34:39 +0900

    feat: 세번째 강의

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/") // 컨트롤러가 정적파일보다 우선순위가 높다
+    public String home() {
+        return "home";
+    }
+}
