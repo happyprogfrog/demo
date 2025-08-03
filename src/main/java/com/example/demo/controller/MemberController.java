42eb2ecda59fa69f5c55512359b2f430a21f249b
happyprogfrog <happyprogfrog@gmail.com>
2025-08-03 22:01:50 +0900

    feat: 네번째 강의

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -3,6 +3,7 @@ package com.example.demo.controller;
 import com.example.demo.domain.Member;
 import com.example.demo.service.MemberService;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,14 +11,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
 
     private final MemberService memberService;
 
-    @Autowired
-    public MemberController(MemberService memberService) {
-        this.memberService = memberService;
-    }
+//    @Autowired
+//    public MemberController(MemberService memberService) {
+//        this.memberService = memberService;
+//    }
 
     @GetMapping("/members/new")
     public String createForm() {
